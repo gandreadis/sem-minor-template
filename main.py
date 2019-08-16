dc911c8a8c6d96c637ed9350554a9b90b53db09b
Georgios Andreadis <info@gandreadis.com>
2019-08-16 17:50:15 +0200

    Add coverage and pylint configs and more instructions

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from project.leap_year_checker import is_leap_year
 
-year = input("Enter a year number: ")
-print("Is", year, "a leap year?", is_leap_year(int(year)))
+
+def main():
+    year = input("Enter a year number: ")
+    print("Is", year, "a leap year?", is_leap_year(int(year)))
+
+
+if __name__ == '__main__':
+    main()
